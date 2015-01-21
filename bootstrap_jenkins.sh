3c9ca657ee6d4e9aa09c565ccc45281960289d99
Patrick Gerken <do3cc@patrick-gerken.de>
2015-01-21 17:02:40 +0100

    Now with developer again

--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -2,5 +2,5 @@ virtualenv --no-setuptools .
 mkdir -p buildout-cache/downloads
 ./bin/python bootstrap.py -v 1.6.3
 ./bin/buildout -N -t 3 -c travis.cfg
-#./bin/develop up -f
+./bin/develop up -f
 # Xvfb :99 -a &
